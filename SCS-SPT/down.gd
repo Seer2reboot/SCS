2007e5aa6fe3edea212dffb4a724b3c6a207a8f7
Furau <code233@126.com>
2024-10-19 04:32:07 +0800

    2024年10月19日04:32:04

---
+++ SCS-SPT/down.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	$"../Sprites".get_child(int($"../Sprites".get_child_count() - 2)).visible = false
+	$"../Sprites".get_child(int($"../Sprites".get_child_count() - 1)).visible = true
+	pass # Replace with function body.
